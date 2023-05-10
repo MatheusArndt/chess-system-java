3f0536c3260e0c6890d2f746ca7f17d128d41714
Matheus Arndt <matheusarndtma1@gmail.com>
2023-05-10 20:36:55 -0300

    Fisrt class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Position op = new Position(2, 6);
 		
+		System.out.println(op);
 		
 	}
 
