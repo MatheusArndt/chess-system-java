b9749d1961b660743acaa3543f3b86cad3874510
Matheus Arndt <matheusarndtma1@gmail.com>
2023-05-12 19:29:08 -0300

    Handling captured pieces

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -35,6 +35,7 @@ public class Rook extends ChessPiece{
 		
 		// left 
 		
+
 		p.setValues(position.getRow(), position.getColumn() - 1);
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
@@ -43,7 +44,7 @@ public class Rook extends ChessPiece{
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 		}
-		
+
 		p.setValues(position.getRow() - 1, position.getColumn());
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
