a7d70d6482788216064f7c1b6b853a3cee6c5cb2
Matheus Arndt <matheusarndtma1@gmail.com>
2023-05-12 18:18:42 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove (ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -47,7 +53,6 @@ public class ChessMatch {
 		}
 		if (!board.piece(position).isThereArryPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
-			
 		}
 	}
 	
