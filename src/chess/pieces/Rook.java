a6a4d04c6af9da108ef7cd26b0987e7505c2c590
Matheus Arndt <matheusarndtma1@gmail.com>
2023-05-15 18:56:57 -0300

    Special move - Castling

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -45,15 +45,6 @@ public class Rook extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow() - 1, position.getColumn());
-		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);
-		}
-		if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
-			mat[p.getRow()][p.getColumn()] = true;
-		}
-		
 		// right
 		
 		p.setValues(position.getRow(), position.getColumn() + 1);
